4ff7b88ea748b58e245333e337db4dfb1d461ac0
Lucas Mezêncio <lucas.mezencio@gmail.com>
2018-08-11 20:42:18 -0300

    Fully revamp the resume

--- js/main.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('body').className = 'fadein';
-    document.querySelector('#year').innerHTML = (new Date()).getFullYear();
-});
